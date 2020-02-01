9b563ae4280a255bece9c7735976ed2a88859fa5
Andrei Radkovich <a.radkovich@godeltech.com>
2020-02-02 01:16:56 +0300

    R programming, week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function creates wrapper over matrix which is able to keep calculated inverted matrix value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <- NULL;
+  set <- function(y) {
+    x <<- y;
+    inv_matrix <- NULL;
+  }
+  get <- function() x
+  get_inv <- function() inv_matrix
+  set_inv <- function(y) inv_matrix <<- y
+  list(
+    set = set,
+    get = get,
+    get_inv = get_inv,
+    set_inv = set_inv
+  )
 }
 
 
-## Write a short comment describing this function
+## computes inverse matrix if it's not computed, otherwise returns value from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- x$get_inv()
+  if(!is.null(inv_matrix)) {
+    return(inv_matrix)
+  }
+  data <- x$get()
+  inv_matrix <- solve(data, ...)
+  x$set_inv(inv_matrix)
+  return(inv_matrix)
 }
